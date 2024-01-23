65d906112af09e806bbf3a8fac6eaab0c05c68ec
Cauê Fernandes Santos <cauesantos359@gmail.com>
2024-01-22 23:24:53 -0300

    condições js

---
+++ aula011/ex008.js
@@ -0,0 +1,8 @@
+var vg = 'Nintendo'
+console.log(`Você está jogando na plataforma ${vg}`)
+if(vg == 'Nintendo'){
+    console.log("Troca de console meu filho!")
+}
+else {
+    console.log("Voce tem bom gosto!")
+}
